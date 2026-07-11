c74b1df51fab006aaaff28b0298690fad151ca4f
leope22 <leope.oliveira@gmail.com>
2026-07-11 20:44:00 -0300

    Adicionando arquivos

--- tcp_co.c
+++ tcp_co.c
@@ -1,26 +1,54 @@
-#include <signal.h>
+#include <signal.h> // Tratamento de interrupções
 #include <stdio.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <arpa/inet.h> // Conversão de endereços de rede
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
-#include <time.h> 
-#include "tcp_co.skel.h"
+#include <time.h>
+#include "tcp_co.skel.h" // Skeleton (ponte entre código BPF e userspace)
 
-struct flow_key { uint32_t src_ip; uint32_t dst_ip; uint16_t src_port; uint16_t dst_port; };
-struct tcp_metrics { uint32_t snd_cwnd; uint32_t ssthresh; uint32_t srtt; uint32_t retransmissions; uint32_t duplicate_acks; uint64_t bytes_acked; uint32_t packets_out; uint32_t retrans_out; uint32_t total_retrans; uint32_t sndbuf; uint8_t tcp_state; char ca_name[16]; };
-struct tcp_event { struct flow_key key; struct tcp_metrics metrics; };
+// Estruturas do mapa - mesmo padrão declarado no kernel:
 
+struct flow_key {
+    uint32_t src_ip;
+    uint32_t dst_ip;
+    uint16_t src_port;
+    uint16_t dst_port;
+};
+
+struct tcp_metrics {
+    uint32_t snd_cwnd;
+    uint32_t ssthresh;
+    uint32_t srtt;
+    uint32_t retransmissions;
+    uint32_t duplicate_acks;
+    uint64_t bytes_acked;
+    uint32_t packets_out;
+    uint32_t retrans_out;
+    uint32_t total_retrans;
+    uint32_t sndbuf;
+    uint8_t  tcp_state;
+    char     ca_name[16];
+};
+
+struct tcp_event {
+    struct flow_key key;
+    struct tcp_metrics metrics;
+};
+
+// Cria o csv para guardar os dados
 FILE *csv_file = NULL; 
 
+// Callback - executado quando o kernel envia um evento pelo Ring Buffer
 static int handle_event(void *ctx, void *data, size_t data_sz) {
     const struct tcp_event *e = data;
     char src_ip[INET_ADDRSTRLEN], dst_ip[INET_ADDRSTRLEN];
-    time_t now = time(NULL); 
+    time_t now = time(NULL);
 
+    // Converte os IPs para string
     inet_ntop(AF_INET, &e->key.src_ip, src_ip, sizeof(src_ip));
     inet_ntop(AF_INET, &e->key.dst_ip, dst_ip, sizeof(dst_ip));
 
+    // Salvar os dados
     if (csv_file) {
         fprintf(csv_file, "%ld,%s,%u,%s,%u,%u,%u,%u,%llu,%u,%u,%s,%u,%u,%u,%u,%u\n",
                 now, src_ip, ntohs(e->key.src_port), dst_ip, ntohs(e->key.dst_port),
@@ -33,6 +61,8 @@ static int handle_event(void *ctx, void *data, size_t data_sz) {
     return 0;
 }
 
+// Tratar a interrupção do programa - sair do loop de polling:
+
 static volatile bool exiting = false;
 
 static void sig_handler(int sig) {
@@ -43,33 +73,48 @@ int main(int argc, char **argv) {
     struct tcp_co_bpf *skel;
     struct ring_buffer *rb = NULL;
     
+    // Definição do nome do csv
     const char *filename = (argc > 1) ? argv[1] : "tcp_co_output.csv";
+    
+    // Cabeçalho
     csv_file = fopen(filename, "w");
     if (csv_file) {
         fprintf(csv_file, "timestamp,src_ip,src_port,dst_ip,dst_port,cwnd,ssthresh,srtt,bytes_acked,state,packets_out,ca_name,retrans_out,total_retrans,sndbuf,retransmissions,duplicate_acks\n");
     }
 
+    // Abre e carrega o programa eBPF no kernel
     skel = tcp_co_bpf__open_and_load();
     if (!skel) return 1;
 
+    // Anexa o programa na função tcp_rcv_established
     tcp_co_bpf__attach(skel);
+    
+    // Configuração do Ring Buffer para o mapa declarado no programa epbf
+    // Dispara a função handle_event a cada evento recebido
     rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
 
     printf("Gravando dados em %s\n", filename);
 
+    // Intercepta os sinais de parada:
+
     signal(SIGINT, sig_handler);
     signal(SIGTERM, sig_handler);
 
+    // Polling - fica checando o Ring Buffer a cada 100ms até a interrupção
     while (!exiting) {
         if (ring_buffer__poll(rb, 100) < 0) break;
     }
+    
+    // Mapa Hash que guarda o último estado conhecido das conexões:
 
     int map_fd = bpf_map__fd(skel->maps.tcp_flows);
     struct flow_key key = {}, next_key;
     struct tcp_metrics metrics;
 
     while (bpf_map_get_next_key(map_fd, &key, &next_key) == 0) {
+
         bpf_map_lookup_elem(map_fd, &next_key, &metrics);
+        
         char src[INET_ADDRSTRLEN], dst[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &next_key.src_ip, src, sizeof(src));
         inet_ntop(AF_INET, &next_key.dst_ip, dst, sizeof(dst));
@@ -99,11 +144,15 @@ int main(int argc, char **argv) {
                    metrics.retransmissions, 
                    metrics.duplicate_acks);
         }
+
         key = next_key;
     }
 
     if (csv_file) fclose(csv_file); 
     ring_buffer__free(rb);
+
+    // Remove o programa do kernel de forma segura
     tcp_co_bpf__destroy(skel);
+    
     return 0;
 }
